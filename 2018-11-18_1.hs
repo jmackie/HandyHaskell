d2d80e9d2a8b24283c3a827bdb78b443af58f180
Jordan Mackie <jmackie@protonmail.com>
2018-11-18 18:11:01 +0000

    Amend 2018-11-18_1.hs

--- 2018-11-18_1.hs
+++ 2018-11-18_1.hs
@@ -1,7 +1,4 @@
 {-# OPTIONS -Weverything -fno-warn-implicit-prelude #-}
-import Control.Monad (when, unless)
-
--- Concurrency stuff:
 import Control.Concurrent
     ( ThreadId
     , forkIO
@@ -16,7 +13,6 @@ import Control.Concurrent.Chan
 import Control.Concurrent.MVar
     ( MVar
     , modifyMVar_
-    , newEmptyMVar
     , newMVar
     , putMVar
     , takeMVar
@@ -46,42 +42,39 @@ slowAction = do
 --
 -- This is useful for not exceeding open file limits, for example.
 concurrently' :: Int -> [IO a] -> IO [a]
+concurrently' _     [] = pure []
 concurrently' limit io = do
-    doneMVar    <- newEmptyMVar   -- done notifier
     runningMVar <- newMVar 0      -- number of running threads
     resultChan  <- newChan        -- results channel
-    throttle limit doneMVar runningMVar resultChan io
-
-    takeMVar doneMVar  -- wait here for everything to finish...
-    results <- getChanContents resultChan
-    pure (take (length io) results)
+    throttle limit runningMVar resultChan io
+    results <- take (length io) <$> getChanContents resultChan
+    pure (forceSpine results)
 
 
-throttle :: Int -> MVar () -> MVar Int -> Chan a -> [IO a] -> IO ()
-throttle _ doneMVar _ _ [] = putMVar doneMVar ()
-throttle limit doneMVar runningMVar resultChan (action : actions) = do
+throttle :: Int -> MVar Int -> Chan a -> [IO a] -> IO ()
+throttle _     _           _          []                 = pure ()
+throttle limit runningMVar resultChan (action : actions) = do
     runningThreads <- takeMVar runningMVar
     if runningThreads < limit
         then do
             putMVar runningMVar (runningThreads + 1)
-            -- ignore thread id
+            -- ignoring thread id
             _ <- runInThread
                 (\a -> do
                     writeChan resultChan a
-                    putChar '.'        -- not needed, just for feedback
                     modifyMVar_ runningMVar (pure . subtract 1)
-                    when (null actions) $ do
-                        putChar '\n'   -- not needed, just for feedback
-                        putMVar doneMVar ()
                 )
                 action
-            unless (null actions) $ do
-                throttle limit doneMVar runningMVar resultChan actions
+            throttle limit runningMVar resultChan actions
         else do
             putMVar runningMVar runningThreads
-            throttle limit doneMVar runningMVar resultChan (action : actions)
+            throttle limit runningMVar resultChan (action : actions)
 
 
 -- | Run an action in a new (lightweight) thread and pass the result to f.
 runInThread :: (a -> IO ()) -> IO a -> IO ThreadId
 runInThread f action = forkIO (action >>= f)
+
+
+forceSpine :: [a] -> [a]
+forceSpine as = foldr (const id) () as `seq` as
